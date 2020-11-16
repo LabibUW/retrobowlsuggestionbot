568de0537461c65f695ad7da90fa26136d577b2d
LabibUW <72846307+LabibUW@users.noreply.github.com>
2020-11-15 17:50:40 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+
+client.once('ready', () => {
+    console.log('RBL Suggestion Bot is online!');
+});
+
+client.on('message', async message => {
+    if(message.channel.name === 'ticket-1'){
+		try {
+            await message.react('⬆️');
+            await message.react('⬇️');
+		} catch (error) {
+			console.error('One of the emojis failed to react.');
+        }
+    }
+});
+
+
+client.login('process.env.BOT_TOKEN');
