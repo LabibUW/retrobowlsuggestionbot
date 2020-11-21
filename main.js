4eabb614014b7792abbc62c48ef8984532db04ad
LabibUW <72846307+LabibUW@users.noreply.github.com>
2020-11-21 15:24:14 -0800

    Update main.js

--- main.js
+++ main.js
@@ -7,33 +7,14 @@ var count = 0;
 
 client.once('ready', () => {
     console.log('RBL Suggestion Bot is online!');
-    client.user.setPresence({
-        status: "online",  // You can show online, idle... Do not disturb is dnd
-        game: {
-            name: "Retro Bowl",  // The message shown
-            type: "PLAYING" // PLAYING, WATCHING, LISTENING, STREAMING,
-        }
-    });
+    client.user.setActivity("Retro Bowl", { type: "Playing" });
 });
 
 client.on('message', async message => {
-    if(message.channel.name === 'community-suggestions' || message.channel.name === 'league-suggestions' || message.channel.name === 'suggestions'){
-		try {
-            await message.react('⬆️');
-            await message.react('⬇️');
+    if(message.channel.name === 'community-suggestions'){
+        count++;
 
-            if(message.content.startsWith("Before posting a new suggestion, please read through the previous suggestions")){
-                message.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error));
-            }
-		} catch (error) {
-			console.error('One of the emojis failed to react.');
-        }
-
-
-        if(message.channel.name == 'community-suggestions'){
-            count++;
-        }
-        if(count == SUGGESTION_MESSAGE_BREAK + 1  && message.channel.name == 'community-suggestions') {
+        if(count == SUGGESTION_MESSAGE_BREAK + 1) {
             channel = client.channels.cache.get('775745410353332294');
             // channel.send('Before posting a new suggestion, please read through the previous suggestions in <#775745410353332294> and <#704484560758374471> to avoid repeat suggestions.\n\nPlease **DO NOT** suggest to play as the defense or an Online mode. ');
 
@@ -45,7 +26,23 @@ client.on('message', async message => {
             count = 0;
         }
     }
+    reactVote('league-suggestions', message);
+    reactVote('suggestions', message);
+    reactVote('community-suggestions', message);
 });
 
+function reactVote(channel, message) {
+    if(message.channel.name === channel){
+		try {
+            message.react('⬆️');
+            message.react('⬇️');
 
-client.login(process.env.BOT_TOKEN);    
+            if(message.content.startsWith("Before posting a new suggestion, please read through the previous suggestions")){
+                message.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error));
+            }
+		} catch (error) {
+			console.error('One of the emojis failed to react.');
+        }
+    }
+}
+client.login(process.env.BOT_TOKEN);        
